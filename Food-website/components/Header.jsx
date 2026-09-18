76c61183a573ecd3afd86d1a88c118a03935d2ac
mishree21 <mishri2109marvaniya@gmail.com>
2026-09-18 12:42:49 +0530

    Create Header.jsx

---
+++ Food-website/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <div className="header">
+            <h1>🍔FoodieExpress</h1>
+
+            <input type="text" placeholder="Search Food..." />
+
+            <button>Cart 🛒</button>
+        </div>
+
+    );
+}
+export default Header;
