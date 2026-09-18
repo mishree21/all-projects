ddca5198c4a895dfbcae0c4537b262c12a6bbfed
mishree21 <mishri2109marvaniya@gmail.com>
2026-09-18 12:40:58 +0530

    Create foodData.js

---
+++ Food-website/data/foodData.js
@@ -0,0 +1,105 @@
+const foods = [
+  {
+    id: 1,
+    name: "Cheese Burger",
+    category: "Burger",
+    price: 149,
+    image:
+      "https://images.unsplash.com/photo-1568901346375-23c9450c58cd",
+  },
+
+  {
+    id: 2,
+    name: "Pepperoni Pizza",
+    category: "Pizza",
+    price: 299,
+    image:
+      "https://images.unsplash.com/photo-1513104890138-7c749659a591",
+  },
+
+  {
+    id: 3,
+    name: "French Fries",
+    category: "Snacks",
+    price: 99,
+    image:
+      "https://images.unsplash.com/photo-1576107232684-1279f390859f",
+  },
+
+  {
+    id: 4,
+    name: "Coca Cola",
+    category: "Drinks",
+    price: 79,
+    image:
+      "https://images.unsplash.com/photo-1690988109029-aa2377d08b12?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29sZCUyMGRyaW5rfGVufDB8fDB8fHww",
+  },
+
+  {
+    id: 5,
+    name: "Garlic Bread",
+    category: "Sandwich",
+    price: 129,
+    image:
+      "https://media.istockphoto.com/id/1136429305/photo/stuffed-garlic-bread.webp?a=1&b=1&s=612x612&w=0&k=20&c=sdNd2n7m7MdESgc7ywWRuVS9StTXInlgaxSqLjnG6rg=",
+  },
+
+  {
+    id: 6,
+    name: "Chocolate Cake",
+    category: "Dessert",
+    price: 199,
+    image:
+      "https://images.unsplash.com/photo-1578985545062-69928b1d9587",
+  },
+
+  {
+    id:7,
+    name:"Margherita Pizza",
+    category:"Pizza",
+    price:200,
+    image:"https://media.istockphoto.com/id/905492462/photo/pizza-margarita-with-mozzarella-cheese-basil-and-tomato-template-for-your-design-and-menu-of.webp?a=1&b=1&s=612x612&w=0&k=20&c=l1udwEUzAkE_Np5_AFy1HNZApX87NHqWe1yOr8O7mYA=",
+  },
+
+  {
+    id:8,
+    name:"Pastry",
+    category:"Dessert",
+    price:100,
+    image:"https://images.unsplash.com/photo-1571115177098-24ec42ed204d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNha2V8ZW58MHx8MHx8fDA%3D",
+  },
+
+  {
+    id:9,
+    name:"Salted French Fries",
+    category:"Snacks",
+    price:100,
+    image:"https://images.unsplash.com/photo-1518013431117-eb1465fa5752?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2FsdGVkJTIwZnJlbmNoJTIwZnJpZXN8ZW58MHx8MHx8fDA%3D",
+  },
+
+  {
+    id:10,
+    name:"Masala Sandwich",
+    category:"Sandwich",
+    price:70,
+    image:"https://images.unsplash.com/photo-1528736235302-52922df5c122?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2hlZXNlJTIwdG9hc3R8ZW58MHx8MHx8fDA%3D",
+  },
+
+  {
+    id:11,
+    name:"Aloo Tikki Burger",
+    category:"Burger",
+    price:150,
+    image:"https://plus.unsplash.com/premium_photo-1664392112262-271039647be9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YWxvbyUyMHRpa2tpJTIwYnVyZ2VyfGVufDB8fDB8fHww",
+  },
+
+  {
+    id:12,
+    name:"Oreo Shake",
+    category:"Drinks",
+    price:200,
+    image:"https://images.unsplash.com/photo-1572490122747-3968b75cc699?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29sZCUyMGRyaW5rfGVufDB8fDB8fHww",
+  },
+];
+
+export default foods;
